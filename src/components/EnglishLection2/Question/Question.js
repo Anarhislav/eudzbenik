08ab85e2e224420d7bb6a2bb2f05cc4f756d57c4
Anarhislav <petardrakulic@Petars-MacBook-Pro.local>
2019-09-23 00:19:36 +0200

    added restart button for english lection

--- src/components/EnglishLection2/Question/Question.js
+++ src/components/EnglishLection2/Question/Question.js
@@ -1,31 +1,67 @@
-import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
-import "./Question.css"
+import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+import "./Question.css";
 class Question extends Component {
-    state = { clicked: false }
-    onClick = ()=> {
-        this.setState({clicked: true})
-    }
+  state = { clicked: false };
+  restart = () => {
+    this.setState({
+      clicked: false
+    });
+  };
+  onClick = () => {
+    this.setState({ clicked: true });
+  };
 
-    render() { 
-        return ( 
-            <div>
-                <div className="english-text-2">Question {this.props.number}</div>
-                <div className="english-text-2">{this.props.question}</div>
-                <div className="buttons">
-                    {this.props.answers.map(element => {
-                        if(this.state.clicked){
-                            if(element == this.props.correctAnswer){
-                                return (<Button key={element}  onClick={this.onClick} color="primary"  variant="contained">{element}</Button>)
-                            }
-                            return (<Button  key={element}  onClick={this.onClick} color="secondary" variant="contained">{element}</Button>)
-                        }
-                        return (<Button  key={element} onClick={this.onClick}  variant="contained">{element}</Button>)
-                    })}
+  render() {
+    return (
+      <div>
+        <div className="q-title">Question {this.props.number}</div>
+        <div className="q-title">{this.props.question}</div>
+        <div className="buttons">
+          {this.props.answers.map(element => {
+            if (this.state.clicked) {
+              if (element == this.props.correctAnswer) {
+                return (
+                  <Button
+                    key={element}
+                    onClick={this.onClick}
+                    color="primary"
+                    variant="contained"
+                  >
+                    {element}
+                  </Button>
+                );
+              }
+              return (
+                <div>
+                  <Button
+                    key={element}
+                    onClick={this.onClick}
+                    color="secondary"
+                    variant="contained"
+                  >
+                    {element}
+                  </Button>
+                 
                 </div>
-            </div>
-         );
-    }
+              );
+            }
+            return (
+              <Button key={element} onClick={this.onClick} variant="contained">
+                {element}
+              </Button>
+            );
+          })}
+        </div>
+        <div className="dugme-rst">
+        <Button  variant="contained" onClick={this.restart}>
+                    Restart
+                  </Button>
+        </div>
+        
+      </div>
+    );
+  }
 }
- 
+
 export default Question;
