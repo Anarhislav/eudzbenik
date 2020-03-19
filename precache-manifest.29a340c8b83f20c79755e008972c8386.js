a016b950d7c384aad79e7108c709769ba98850c9
Petar Drakulic <petardrakulic@Petars-MacBook-Pro.local>
2020-03-19 18:53:53 +0100

    Updates

---
+++ precache-manifest.29a340c8b83f20c79755e008972c8386.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "f75b8024248a572e64a1",
+    "url": "/eudzbenik/static/css/main.e87e693a.chunk.css"
+  },
+  {
+    "revision": "f75b8024248a572e64a1",
+    "url": "/eudzbenik/static/js/main.f75b8024.chunk.js"
+  },
+  {
+    "revision": "3df1704f6386da047c4e",
+    "url": "/eudzbenik/static/js/1.3df1704f.chunk.js"
+  },
+  {
+    "revision": "7237e4cfe9c3e576265b",
+    "url": "/eudzbenik/static/js/runtime~main.7237e4cf.js"
+  },
+  {
+    "revision": "fe224a5f344dc145505ee1c542e6f70a",
+    "url": "/eudzbenik/static/media/family.fe224a5f.ogg"
+  },
+  {
+    "revision": "b42afe9cf8acc9eea60b4a0cb2c5e4eb",
+    "url": "/eudzbenik/static/media/detlicn.b42afe9c.gif"
+  },
+  {
+    "revision": "e98adad31e777bb02ebf2c0302f0c61c",
+    "url": "/eudzbenik/static/media/divlja svinjan.e98adad3.gif"
+  },
+  {
+    "revision": "bf300f5c09279a76a2d6774424421cb9",
+    "url": "/eudzbenik/static/media/kokoskan.bf300f5c.gif"
+  },
+  {
+    "revision": "a3b342f94d0db18e869e488e6abd43aa",
+    "url": "/eudzbenik/static/media/konjn.a3b342f9.gif"
+  },
+  {
+    "revision": "90bbeb1c85174d36099041b7e069712f",
+    "url": "/eudzbenik/static/media/kornjacan.90bbeb1c.gif"
+  },
+  {
+    "revision": "a9195783a8e534793dde4b3433e00b42",
+    "url": "/eudzbenik/static/media/kozan.a9195783.gif"
+  },
+  {
+    "revision": "de28ef0e07f39952e10618658a18e1d0",
+    "url": "/eudzbenik/static/media/part1.de28ef0e.png"
+  },
+  {
+    "revision": "b724d6af91ba8fcbbf983b617cba477c",
+    "url": "/eudzbenik/static/media/part2.b724d6af.png"
+  },
+  {
+    "revision": "72f1e5823545e285f49e693c1e9d0c34",
+    "url": "/eudzbenik/static/media/page1.72f1e582.png"
+  },
+  {
+    "revision": "9ba8eafd1381355f406ec4d8582da351",
+    "url": "/eudzbenik/static/media/farm1.9ba8eafd.png"
+  },
+  {
+    "revision": "a2dfeab58e5960db1a6d3328dda9d244",
+    "url": "/eudzbenik/static/media/farm2.a2dfeab5.png"
+  },
+  {
+    "revision": "67efdce992b19b5293f4520f86739939",
+    "url": "/eudzbenik/static/media/bg1.67efdce9.png"
+  },
+  {
+    "revision": "207f5b294871710c86f7697c7d86a7c2",
+    "url": "/eudzbenik/static/media/bg2.207f5b29.png"
+  },
+  {
+    "revision": "ad99bd7d049524d5dab76ba9d163cb2b",
+    "url": "/eudzbenik/static/media/mathPage1.ad99bd7d.png"
+  },
+  {
+    "revision": "ad99bd7d049524d5dab76ba9d163cb2b",
+    "url": "/eudzbenik/static/media/mathPage.ad99bd7d.png"
+  },
+  {
+    "revision": "b805be63609791496028e3302cafa890",
+    "url": "/eudzbenik/index.html"
+  }
+];
